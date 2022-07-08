ec64f18f2fdaee3d217ac24186d6e72ac46da652
su neutralidad <kolivaresrojas@gmail.com>
2022-07-08 14:11:57 -0400

    init

---
+++ core/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from .models import *
+
+# Register your models here.
+admin.site.register(Categoria)
+admin.site.register(Vehiculo)
+admin.site.register(Productos)
+admin.site.register(Promocion)
